5bab2a01d7ce2dea4b0b4484930413b1a6316f97
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-03-12 21:10:41 +0300

    practika управление элементами на странице

--- MoviSite/js/script.js
+++ MoviSite/js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 /* Задания на урок:
 
 1) Удалить все рекламные блоки со страницы (правая часть сайта)
@@ -12,19 +13,22 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+
 
 const movieDB = {
     movies: [
         "Логан",
         "Лига несправедливости",
         "Ла-бла лэнд",
-        "Одержимость",
+        "Недержимость",
         "Скотт Пилигрим против..."
     ]
 };
-// убрать рекламу
-document.querySelector('.promo__adv').style.display = 'none';
+// убрать рекламу (скрыть)
+// document.querySelector('.promo__adv').style.display = 'none';
+// убрать рекламу (удалить, только рекламные блоки)
+const adv = document.querySelectorAll('.promo__adv img');
+adv.forEach(item => item.remove());
 // Изменить жанр фильма, поменять "комедия" на "драма"
 const genre = document.querySelector('.promo__genre');
 genre.textContent = 'ДРАМА';
@@ -32,10 +36,26 @@ genre.textContent = 'ДРАМА';
 //Изменить задний фон постера с фильмом на изображение "bg.jpg".Оно лежит в папке img.
 //Реализовать только при помощи JS
 const backgroundPromo = document.querySelector('.promo__bg');
-backgroundPromo.style.background = `url("./img/bg.jpg") no-repeat center center/cover`;
+backgroundPromo.style.backgroundImage = `url("./img/bg.jpg")`;
+// backgroundPromo.style.background = 'center/cover;';
 
 // Список фильмов на странице сформировать на основании данных из этого JS файла.
 // Отсортировать их по алфавиту
-movieDB.movies.sort();
+// добавить нумерацию фильмов
+// * мой рабочий метод)
+
 const listOfFilms = document.querySelectorAll('.promo__interactive-item');
+movieDB.movies.sort();
 listOfFilms.forEach((item, index) => item.textContent = `${index + 1}. ${movieDB.movies[index]} `);
+
+//? метод Петреченко (сложнее)
+// const listOfFilms = document.querySelector('.promo__interactive-list');
+// listOfFilms.innerHTML = '';
+// movieDB.movies.sort();
+// movieDB.movies.forEach((item, i) => {
+//     listOfFilms.innerHTML += `
+//         <li class="promo__interactive-item">${i+1}. ${item}
+//             <div class="delete></div>
+//         </li>
+//     `;
+// });
