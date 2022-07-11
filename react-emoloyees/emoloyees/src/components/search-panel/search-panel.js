9cff580a66946cde9d8473622f0832a58733c017
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-07-11 21:49:28 +0300

    React.createRef

--- react-emoloyees/emoloyees/src/components/search-panel/search-panel.js
+++ react-emoloyees/emoloyees/src/components/search-panel/search-panel.js
@@ -1,31 +1,54 @@
 import './search-panel.css';
 import {Component} from "react";
+import React from "react";
 
 class SearchPanel extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            term: ''
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: ''
+		}
+		// this.myRef = React.createRef(); // ! стандартный способ установить
+	}
 
-    onSearch = (e) => {
-        const term = e.target.value;
-        this.setState({term});
-        this.props.onUpdateSearch(term);
-    }
+	setInputRef = elem => {
+		this.myRef = elem;
+	}
 
-    render() {
-        return (
-            <input
-                type={'text'}
-                className={'form-control search-input'}
-                placeholder={'Найти'}
-                value={this.state.term}
-                onChange={this.onSearch}
-            />
-        )
-    }
+	focusInput = () => {
+		if (this.myRef) {
+			this.myRef.focus();
+		}
+
+	}
+
+
+// componentDidMount() {
+//
+// }
+
+onSearch = (e) => {
+	const term = e.target.value;
+	this.setState({term});
+	this.props.onUpdateSearch(term);
+}
+
+render()
+{
+	return (
+		<div>
+			<input
+				ref={this.setInputRef}
+				type={'text'}
+				className={'form-control search-input'}
+				placeholder={'Найти'}
+				value={this.state.term}
+				onChange={this.onSearch}
+			/>
+			<textarea onClick={this.focusInput}></textarea>
+		</div>
+	)
+}
 }
 
 export default SearchPanel;
