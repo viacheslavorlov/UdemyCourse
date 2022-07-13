a958edd0dfc5b09b04076c27677b4531865b9f5d
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-07-13 21:05:22 +0300

    ReactDOM.createPortal - работает

--- react-emoloyees/emoloyees/src/components/search-panel/search-panel.js
+++ react-emoloyees/emoloyees/src/components/search-panel/search-panel.js
@@ -1,6 +1,7 @@
 import './search-panel.css';
 import {Component} from "react";
 import React from "react";
+import ReactDOM from "react-dom";
 
 class SearchPanel extends Component {
 	constructor(props) {
@@ -8,47 +9,75 @@ class SearchPanel extends Component {
 		this.state = {
 			term: ''
 		}
-		// this.myRef = React.createRef(); // ! стандартный способ установить
+		this.myRef = React.createRef(); // ! стандартный способ установить
 	}
 
-	setInputRef = elem => {
-		this.myRef = elem;
-	}
-
+	// setInputRef = elem => {
+	// 	this.myRef = elem;
+	// }
+	//
 	focusInput = () => {
 		if (this.myRef) {
-			this.myRef.focus();
+			this.myRef.current.focus();//переводит фокус обратно на элемент с рефом
 		}
 
 	}
 
 
-// componentDidMount() {
-//
-// }
+	componentDidMount() {
+		this.myRef.current.focus();
+	}
 
-onSearch = (e) => {
-	const term = e.target.value;
-	this.setState({term});
-	this.props.onUpdateSearch(term);
+	onSearch = (e) => {
+		const term = e.target.value;
+		this.setState({term});
+		this.props.onUpdateSearch(term);
+	}
+
+	showMeItWorks = () => {
+		console.log('click!'); // будет работать на содержимом портала! подобно делигированию событий
+	}
+
+	render() {
+		return (
+			<div onClick={this.showMeItWorks}>
+				<input
+					ref={this.myRef}
+					type={'text'}
+					className={'form-control search-input'}
+					placeholder={'Найти'}
+					value={this.state.term}
+					onChange={this.onSearch}
+				/>
+				<textarea onClick={this.focusInput}></textarea>
+				<Portal>
+					<Text/>
+				</Portal>
+
+			</div>
+		)
+	}
 }
 
-render()
-{
+const Portal = (props) => {
+	const node = document.createElement('div');
+	document.body.appendChild(node);
+
+	return ReactDOM.createPortal(props.children, node);
+}
+
+const Text = () => {
 	return (
 		<div>
-			<input
-				ref={this.setInputRef}
-				type={'text'}
-				className={'form-control search-input'}
-				placeholder={'Найти'}
-				value={this.state.term}
-				onChange={this.onSearch}
-			/>
-			<textarea onClick={this.focusInput}></textarea>
+			<p
+			style={{backgroundColor: 'red',
+					position: 'absolute',
+					right: '50%',
+					top: '10%'}}
+			>HELLO!</p>
 		</div>
 	)
-}
+
 }
 
 export default SearchPanel;
